f81c2a0b8269394c2c01f2c8213c282118bb122e
Jonne C C Silva <jonne.cley@gmail.com>
2026-06-26 21:48:05 -0300

    feat: adiciona rotas iniciais no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './router'
+
 function App() {
-  return (
-    <> 
-      <h1>Welcome to Vite</h1>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
