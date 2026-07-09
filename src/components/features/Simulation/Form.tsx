a34ae6214f3000e84b33e8d2ae1f35e8647c911d
Jonne C C Silva <jonne.cley@gmail.com>
2026-07-08 22:48:50 -0300

    Adicionando Progresso ao Formulário de Simulação

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,8 +1,9 @@
+import { StepProgress } from './Progress'
+
 export const SimulationForm = () => {
   return (
-    <div>
-      <h2>Simulation Form</h2>
-      {/* Form content goes here */}
-    </div>
+    <>
+      <StepProgress currentStep={1} totalSteps={6} />
+    </>
   )
 }
