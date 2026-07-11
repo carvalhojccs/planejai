d414a2c5d97d0ee3fc106fe4f3418d748741ae55
Jonne C C Silva <jonne.cley@gmail.com>
2026-07-10 23:01:52 -0300

    Criando o Card de Pergunta e o Input do Formulário

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,22 @@
+import { PiggyBank } from 'lucide-react'
+
+import FormStep from './FormStep'
 import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
   return (
     <>
       <StepProgress currentStep={1} totalSteps={6} />
+      <FormStep
+        icon={PiggyBank}
+        title="Identificação"
+        question="Qual é o seu nome completo?"
+        inputProps={{
+          placeholder: 'Digite seu nome completo',
+          prefix: 'Nome',
+          type: 'text',
+        }}
+      />
     </>
   )
 }
