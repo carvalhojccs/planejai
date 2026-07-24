11732a7c24495ee5a012c06dfb5c7a632f0776e9
Jonne C C Silva <jonne.cley@gmail.com>
2026-07-23 22:26:38 -0300

    Recuperando Simulações pelo ID na Página de Resultado

--- src/router.tsx
+++ src/router.tsx
@@ -17,7 +17,7 @@ export const router = createBrowserRouter([
         ),
       },
       {
-        path: '/resultado',
+        path: '/resultado/:id',
         element: <SimulationResultsPage />,
       },
       {
